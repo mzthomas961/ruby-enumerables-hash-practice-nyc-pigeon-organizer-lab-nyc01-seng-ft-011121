9a2b08def7d998b6ea7b121dcca1cfae2f867dbd
Matthew Thomas <mzthomas96@gmail.com>
2021-01-07 04:52:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  data.each do |key, value| #key: color, gender, lives. #value: hash
+  data.each do |key, value| 
     value.each do |describe, array| 
-      array.each do |name| #name: index inside aray
+      array.each do |name| 
         if pigeon_list.has_key?(name)
           if pigeon_list[name].has_key?(key)
          
